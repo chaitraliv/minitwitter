2dd1ffa66e4c44b397964f45d5cea03a4fe513db
chaitraliv <chaitrali.vaidya@instazen.com>
2020-08-20 16:37:20 +0530

    added comments

--- backend/instatwitter/main/views.py
+++ backend/instatwitter/main/views.py
@@ -12,43 +12,51 @@ from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def login_api(request):
+    #retrive the username and password from request.
     userName = request.data['userName']
     passWord = request.data['passWord']
     
-    if userName is None or passWord is None:
+    if userName is None or passWord is None:            #if fields are blank
         return Response(status= status.HTTP_400_BAD_REQUEST)
 
-    user = authenticate(username=userName, password=passWord)
-    if user is None:
-        return Response(data={'message':'Wrong or invalid login credentials '}, status= status.HTTP_404_NOT_FOUND)
+    user = authenticate(username=userName, password=passWord)           #check if the login credentials are correct
+
+    if user is None:            #if wrong login credentials
+        return Response(status= status.HTTP_404_NOT_FOUND)
     
-    # serializer = UserSerializer(user)
-    return Response(data={'message' : 'Welcome'},status=status.HTTP_200_OK)
+   #login is successful
+    return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def registration_api(request):
+    #retrive data from request
     username = request.data['username']
     password = request.data['password']
     firstname = request.data['firstname']
     lastname = request.data['lastname']
     email = request.data['email']
 
+    #if any field is empty, raise error 
     if username is None or password is None or firstname is None or lastname is None or email is None:
-        return Response(status= status.HTTP_205_RESET_CONTENT)
+        return Response(status= status.HTTP_400_BAD_REQUEST)
 
+    #check if username already exists
     try:
         user = User.objects.get(username=username)
 
-    except:
+    except:         #if username is valid,save that information for that user 
         user = User.objects.create_user(username= username,
                                  email= email,
                                  password= password,
                                  first_name = firstname,
                                  last_name = lastname)
         user.save()
-        token, created = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)         #create token for the user
         data = {}
         data['token']=token.key 
-        return Response(data, status= status.HTTP_201_CREATED)
-    return Response(status= status.HTTP_226_IM_USED)
+        serializer = UserSerializer(data)
+
+        return Response(serializer, status= status.HTTP_201_CREATED)        #user is created
+
+    return Response(serializer.errors,status= status.HTTP_226_IM_USED)          #user already taken 

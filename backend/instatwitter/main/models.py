d8e6d8908b16363d6b08df6c8e8a6fa6ce5885f0
chaitraliv <chaitrali.vaidya@instazen.com>
2020-08-31 14:28:53 +0530

    final changes

--- backend/instatwitter/main/models.py
+++ backend/instatwitter/main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserData(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     userbio = models.CharField(max_length=100)
     
 class TweetData(models.Model):
